6d78b8ede6c359b43c8b9274039ed65359d44b19
longbiu <1679648995@qq.com>
2021-09-02 07:04:20 +0800

    can't run sidekiq web

--- Gemfile
+++ Gemfile
@@ -1,3 +1,5 @@
 source "https://rubygems.org"
 
-gem "sidekiq"
+gem 'sidekiq', '~> 6.1.3'
+gem "rack"
+gem "sinatra"
---
+++ worker01.rb
@@ -0,0 +1,28 @@
+require 'sidekiq'
+
+Sidekiq.configure_client do |config|
+  config.redis = { db: 1 }
+end
+
+Sidekiq.configure_server do |config|
+  config.redis = { db: 1 }
+end
+
+class OurWorker
+  include Sidekiq::Worker
+
+  def perform(complexity)
+    case complexity
+    when "super_hard"
+      puts "Chargign a credit card……"
+      raise "Woops stuff got bad"
+      puts "Really took quite a bit of effort"
+    when "hard"
+      sleep 10
+      puts "That was a bit of work"
+    else
+      sleep 1
+      puts "That wasn't a lot of effort"
+    end
+  end
+end
